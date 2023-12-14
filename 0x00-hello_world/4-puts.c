b8f7b8d5a006d23a5dd7efb595df1059d0de6180
asheetuu <ashetubulcha@gmail.com>
2023-12-14 04:10:35 -0800

    puts commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * This program is going to display the text 
+ * to the screen
+ */
+int main()
+
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
